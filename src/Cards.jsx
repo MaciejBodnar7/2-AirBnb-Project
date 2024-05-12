cc94dddfa2530fb24e4d1551d2f141628738f995
MaciejBodnar7 <maciej.bodnar2@gmail.com>
2024-05-12 15:45:15 +0200

    fisnish styling cards

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -3,15 +3,24 @@ import "./Cards.css"
 function Cards() {
   return (
     <>
-      <section className="cards-section w-11/12 mx-auto mt-16">
-        <div className="bg-slate-500 w-1/4 h-5/6">
-          <div className="w-full h-full relative">
+      <section className="cards-section w-11/12 mx-auto mt-16 text-gray-900">
+        <div className=" w-1/4 h-5/6">
+          <div className="w-full relative">
             <p className="bg-slate-200 w-fit absolute mt-2 ml-2 py-1 px-2 text-sm rounded-lg">SOLD OUT</p>
             <img src="../src/assets/image 12.png" alt="" className="w-full" />
           </div>
-          <div>
-            <p>test</p>
+          <div className="flex mt-3">
             <img src="../src/assets/Star 1.png" alt="" />
+            <p className="ml-2">
+              5.0 <span className="text-gray-400">(6)</span>
+            </p>
+            <p className="text-gray-400 ml-2">- USA</p>
+          </div>
+          <div>
+            <p className="mt-2">Life lessons with Katie Zaferes</p>
+            <p className="font-bold mt-2">
+              From <span>$146</span> <span className="font-normal">/person</span>
+            </p>
           </div>
         </div>
       </section>
